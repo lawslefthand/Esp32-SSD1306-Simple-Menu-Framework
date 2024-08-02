e6167dfee973b6da0b022b7ff649be0e46b40a6f
Aryan Basnet <98141627+lawslefthand@users.noreply.github.com>
2024-08-02 12:53:08 +0530

    Updated ssd1306_final.c
    Added newmenu return task which shows menu by using a dedicated gpio.

--- ssd1306_final.c
+++ ssd1306_final.c
@@ -12,6 +12,7 @@
 #include "hal/i2c_types.h"
 #include "hal/i2c_types.h"
 #include "portmacro.h"
+#include "soc/gpio_num.h"
 #include "ssd1306.h"
 #include "driver/gpio.h"
 
@@ -25,7 +26,9 @@ TaskHandle_t handle3 = NULL;
 TaskHandle_t handle4 = NULL;
 TaskHandle_t handle5 = NULL;
 TaskHandle_t handle6 = NULL;
-TaskHandle_t menu = NULL;
+TaskHandle_t menu_str = NULL;
+TaskHandle_t menu_rtr = NULL;
+
 
 static const char *TAG = "my_tag";
 
@@ -137,11 +140,11 @@ void menu_init(void *arg)
     ssd1306_clear_screen(&dev, false);
     ssd1306_init(&dev, 128, 64);
     ssd1306_contrast(&dev, 0xff);
-    ssd1306_display_text(&dev, 0, "Option 1", 11, false);
-    ssd1306_display_text(&dev, 1, "Option 2", 11, false);
-    ssd1306_display_text(&dev, 2, "Option 3", 11, false);
-    ssd1306_display_text(&dev, 3, "Option 4", 11, false);
-
+    ssd1306_display_text(&dev, 0, "Gpio x for Option 1", 20, false);
+    ssd1306_display_text(&dev, 1, "Gpio y for Option 2", 20, false);
+    ssd1306_display_text(&dev, 2, "Gpuo z for Option 3", 20, false);
+    ssd1306_display_text(&dev, 3, "Gpio a for Option 4", 20, false); 
+	 
     while (1)
     {
         ESP_LOGI(TAG, "Menu is still running");
@@ -150,6 +153,7 @@ void menu_init(void *arg)
         int gpio_35 = gpio_get_level(35);
         int gpio_34 = gpio_get_level(34);
         int gpio_39 = gpio_get_level(39);
+        int gpio_32 = gpio_get_level(32);
 
         printf("GPIO 36: %d\n", gpio_36);
         vTaskDelay(400 / portTICK_PERIOD_MS);
@@ -167,7 +171,8 @@ void menu_init(void *arg)
             if (handle4) vTaskDelete(handle4);
             if (handle5) vTaskDelete(handle5);
             if (handle6) vTaskDelete(handle6);
-            handle2 = handle3 = handle4 = handle5 = handle6 = NULL;
+            if (menu_rtr) vTaskDelete(handle6);
+        menu_rtr = handle2 = handle3 = handle4 = handle5 = handle6 = NULL;
         } else if (gpio_36 == 1 && handle2 == NULL) {
             xTaskCreatePinnedToCore(&task_2, "Task 2", 4096, NULL, 5, &handle2, 1);
             if (handle1) vTaskDelete(handle1);
@@ -175,7 +180,8 @@ void menu_init(void *arg)
             if (handle4) vTaskDelete(handle4);
             if (handle5) vTaskDelete(handle5);
             if (handle6) vTaskDelete(handle6);
-            handle1 = handle3 = handle4 = handle5 = handle6 = NULL;
+            if (menu_rtr) vTaskDelete(handle6);
+          menu_rtr =  handle1 = handle3 = handle4 = handle5 = handle6 = NULL;
         } else if (gpio_35 == 1 && handle3 == NULL) {
             xTaskCreatePinnedToCore(&task_3, "Task 3", 4096, NULL, 5, &handle3, 1);
             if (handle1) vTaskDelete(handle1);
@@ -183,7 +189,8 @@ void menu_init(void *arg)
             if (handle4) vTaskDelete(handle4);
             if (handle5) vTaskDelete(handle5);
             if (handle6) vTaskDelete(handle6);
-            handle1 = handle2 = handle4 = handle5 = handle6 = NULL;
+            if (menu_rtr) vTaskDelete(handle6);
+        menu_rtr = handle1 = handle2 = handle4 = handle5 = handle6 = NULL;
         } else if (gpio_34 == 1 && handle4 == NULL) {
             xTaskCreatePinnedToCore(&task_4, "Task 4", 4096, NULL, 5, &handle4, 1);
             if (handle1) vTaskDelete(handle1);
@@ -191,21 +198,63 @@ void menu_init(void *arg)
             if (handle3) vTaskDelete(handle3);
             if (handle5) vTaskDelete(handle5);
             if (handle6) vTaskDelete(handle6);
-            handle1 = handle2 = handle3 = handle5 = handle6 = NULL;
-        }
+            if (menu_rtr) vTaskDelete(handle6);
+         menu_rtr =  handle1 = handle2 = handle3 = handle5 = handle6 = NULL;
+        } else if (gpio_32 == 1 && menu_rtr) {
+			xTaskCreatePinnedToCore(&task_4, "Menu return", 4096, NULL, 5, &menu_rtr, 1);
+            if (handle1) vTaskDelete(handle1);
+            if (handle2) vTaskDelete(handle2);
+            if (handle3) vTaskDelete(handle3);
+            if (handle5) vTaskDelete(handle5);
+            if (handle6) vTaskDelete(handle6);
+           handle1 = handle2 = handle3 = handle5 = handle6 = NULL;
+		}
 
         vTaskDelay(pdMS_TO_TICKS(100));
     }
      vTaskDelay(pdMS_TO_TICKS(100)); 
 }
 
+void menu_return(void *arg)
+{
+	if(xSemaphoreTake(xMutex, portMAX_DELAY))
+	{
+	 ESP_LOGI(TAG, "Menu rtr is running");
+     i2c_driver_delete(I2C_NUM_0);
+     vTaskDelay(1000/portTICK_PERIOD_MS);
+     SSD1306_t dev;
+     i2c_master_init(&dev, 21, 22, 27);
+     ssd1306_clear_screen(&dev, false);
+     ssd1306_init(&dev, 128, 64);
+     ssd1306_contrast(&dev, 0xff);
+     xSemaphoreGive(xMutex);	
+     while(1)
+     {
+		ssd1306_display_text(&dev, 0, "Gpio x for Option 1", 20, false);
+        ssd1306_display_text(&dev, 1, "Gpio y for Option 2", 20, false);
+        ssd1306_display_text(&dev, 2, "Gpuo z for Option 3", 20, false);
+        ssd1306_display_text(&dev, 3, "Gpio a for Option 4", 20, false); 
+	 }
+	}
+}
+
+
 void app_main()
 {
+	//gpio configuration
     gpio_set_direction(35, GPIO_MODE_INPUT);
     gpio_set_direction(36, GPIO_MODE_INPUT);
     gpio_set_direction(39, GPIO_MODE_INPUT);
     gpio_set_direction(35, GPIO_MODE_INPUT);
+    gpio_config_t io_conf;
+    io_conf.intr_type= GPIO_INTR_DISABLE;
+    io_conf.mode=GPIO_MODE_INPUT;
+    io_conf.pin_bit_mask = (1ULL<< GPIO_NUM_32);
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    gpio_config(&io_conf);
+    
     xMutex = xSemaphoreCreateMutex();
     xSemaphoreGive(xMutex);
-    xTaskCreatePinnedToCore(&menu_init, "Main menu task", 4092 * 2, NULL, 6, &menu, 0);
+    xTaskCreatePinnedToCore(&menu_init, "Main menu task", 4092 * 2, NULL, 6, &menu_str, 0);
 }
